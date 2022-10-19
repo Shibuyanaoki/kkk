1a0245a06bed706c95bcefe2a0114b1f463a86f5
Naoshi2772@icloud.com <Naoshi2772@icloud.com>
2022-10-19 15:25:23 +0900

    2行目をConflitに変更

--- kkk.cpp
+++ kkk.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello Git Hub !\n";
-    std::cout << "Hello MastarBranch !\n";
+    std::cout << "Hello ConflitBranch !\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
